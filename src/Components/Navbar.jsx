79e5690ce2cc96ce8799cb8cf00b202f7d6ae1e8
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-29 03:54:02 +0530

    add li and logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Navbar.css'
+import logo  from '../assets/logo.png';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
+      <img src={logo} alt="Arrow" />
+      <ul className='nav-menu'>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
     
     </div>
   )
