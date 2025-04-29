b4c421745a19c940c348a506cc435756365e24a0
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-29 18:04:55 +0530

    add some code in hero file

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,12 @@ const Hero = () => {
   return (
     <div className='hero'>
       <img src={profile} alt="profile" />
-      <h1>I'm Guddiya Singh , frontend devloper based in India</h1>
+      <h1>I'm Guddiya Singh , frontend devloper based in India.</h1>
+      <p>I am a frontend developer from Bangalore, India </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My eesume</div>
+      </div>
     </div>
   )
 }
