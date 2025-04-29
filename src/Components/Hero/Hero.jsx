231277e18bed3e929ccd5250be72436145d0e132
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-30 01:16:27 +0530

    add some css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='hero'>
       <img src={profile} alt="profile" />
-      <h1>I'm Guddiya Singh , frontend devloper based in India.</h1>
+      <h1><span>I'm Guddiya Singh</span>frontend devloper based in India.</h1>
       <p>I am a frontend developer from Bangalore, India </p>
       <div className="hero-action">
         <div className="hero-connect">Connect with me</div>
