b3bba6ce66de52e6c22615e59f2afad21278bcb1
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-29 17:58:07 +0530

    add hero file and font links

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import profile  from '../../assets/profile.jpg';
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={profile} alt="profile" />
+      <h1>I'm Guddiya Singh , frontend devloper based in India</h1>
+    </div>
+  )
+}
+
+export default Hero
