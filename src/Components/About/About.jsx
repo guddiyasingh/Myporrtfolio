d265650b2e1cc9f258d92c51c7e893e22b534c31
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-05-03 22:47:05 +0530

    add css in about file

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -29,9 +29,23 @@ const About = () => {
       </div>
       <div className="about-achievements">
         <div className="about-achievement">
-          
+          <h1>10+</h1>
+          <p>YEARS OF EXPERIENCE</p>
         </div>
-      </div>
+        <hr />
+        
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>YEARS OF EXPERIENCE</p>
+        </div>
+        <hr />
+            
+        </div>
     </div>
   )
 }
